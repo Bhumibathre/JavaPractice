161f71766844ee796b07bd8722538ff2d554623a
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-02 23:32:13 +0530

    Add files via upload

---
+++ JavaBasics2.java
@@ -0,0 +1,8 @@
+public class JavaBasics2 {
+   public static void main (String args[])
+   {
+    System.out.print("1 2 3 4 5\n");
+    System.out.print("1 2 3 4 5\n");
+    System.out.print("1 2 3 4 5\n");
+   }
+}
