92e68ea0989fbfd32c993a11b152dd26d6a5667f
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-03 11:19:14 +0530

    Add files via upload

---
+++ JavaBasics5.java
@@ -0,0 +1,16 @@
+public class JavaBasics5 {
+    public static void main(String args[]) {
+        int a=10;
+        float condition=(float)34.6;
+        String name= "Bhumi Bathre";
+
+
+        System.out.println(a);
+        System.out.println(condition);
+        System.out.println(name);
+        a = 50;
+        System.out.println(a);
+    }
+    
+
+}
