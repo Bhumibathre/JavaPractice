11f9d559a8b6567135bbee3570f8137d3511d00a
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-03 11:19:35 +0530

    Add files via upload

---
+++ JavaBasics6.java
@@ -0,0 +1,12 @@
+public class JavaBasics6 {
+    public static void main(String args [])
+
+    {
+        int a = 45;
+        int b = 55;
+
+        b=a;
+        System.out.println(b);
+
+    }
+}
