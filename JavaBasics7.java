4e076f42b14d334d3f639f3441279f0ef56e8eb3
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-04 18:07:06 +0530

    Add files via upload

---
+++ JavaBasics7.java
@@ -0,0 +1,22 @@
+public class javabasics6 {
+    public static void main(String args[]){
+
+        byte b= 100;
+        System.out.println(b);
+        char ch='a';
+        System.out.println(ch);
+        boolean dec = true;
+        System.out.println(dec);
+        float pyc=  3.6f ;
+        System.out.println(pyc);
+        int number=89;
+        System.out.println(number);
+        double db = 56.78;
+        System.out.println(db);
+        long lg = 78;
+        System.out.println(lg);
+        short sh=78;
+        System.out.println(sh);
+
+    }
+}
