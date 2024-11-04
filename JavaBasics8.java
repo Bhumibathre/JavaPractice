b9d7e341338a51c1f3ad571e1ca58e878af8def5
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-04 18:07:22 +0530

    Add files via upload

---
+++ JavaBasics8.java
@@ -0,0 +1,11 @@
+public class javabasics8 {
+    public static void main(String args[]){
+
+        int a = 67;
+        int b = 45;
+        int sum = a+b ;
+        System.out.println(sum);
+        
+
+    }
+}
