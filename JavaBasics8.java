b7c59a96244c030dfe6f465061046f6856abedca
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-12 00:29:13 +0530

    JavaBasics8.java

--- JavaBasics8.java
+++ JavaBasics8.java
@@ -2,7 +2,7 @@ public class javabasics8 {
     public static void main(String args[]){
 
         int a = 67;
-        int b = 45;
+        int b = 45; 
         int sum = a+b ;
         System.out.println(sum);
         
