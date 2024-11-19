ade73a37768e68d8db546db3be7d92ea11ee94fc
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-19 11:31:51 +0530

    Update JavaBasics4.java

--- JavaBasics4.java
+++ JavaBasics4.java
@@ -3,7 +3,7 @@ public class JavaBasics4 {
         int a = 10;   
         int b = 20; 
         System.out.println(a);  
-        System.out.println(b);
+        System.out.println(b); 
         
     }
 
