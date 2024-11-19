ffd2ee8289c12f4471e1c2c03c2dcdfaa7b446ec
Bhumi Bathre <144484222+Bhumibathre@users.noreply.github.com>
2024-11-19 11:31:39 +0530

    Update JavaBasics4.java

--- JavaBasics4.java
+++ JavaBasics4.java
@@ -1,5 +1,5 @@
 public class JavaBasics4 {  
-    public static void main(String args[]) {     
+    public static void main(String args[]) {      
         int a = 10;   
         int b = 20; 
         System.out.println(a);  
